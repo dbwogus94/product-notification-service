419f84bc32642aa2ba4159e564ec95f6d62f7e2d
jae hyun <1994dbwogus@gmail.com>
2023-08-01 21:35:39 +0900

    feat: user app 초기 설정 완료

--- apps/user/src/app.module.ts
+++ apps/user/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppConfig, LocalConfig } from './app-config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [() => AppConfig.validate(LocalConfig)],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
